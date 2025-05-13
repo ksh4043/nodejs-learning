29473921c98c9a16f8f9040244e4fadd573c37f9
ksh <rlatlgus925@naver.com>
2025-05-13 20:08:21 +0900

    v0.2 Express

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var figlet = require("figlet");
+const express = require("express") // Express 모듈 불러오기
+const app = express() // Express 앱 객체 생성
 
-figlet("Hello World!!", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+app.get('/', (req, res) => { // Get 요청 처리
+  res.send('Hello World') // 클라이언트에 응답
+})
+
+app.listen(3000)            // 3000번 포트에서 서버 실행
